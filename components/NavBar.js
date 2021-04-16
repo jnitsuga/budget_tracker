d59409d87a7617c433d25af7c53826e74787ac94
jango <jangoagustin18@gmail.com>
2021-04-16 21:08:00 +0800

    building the client components

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import { Nav, Navbar  } from 'react-bootstrap'
+import Link from 'next/link'
+
+export default () => {
+  return (
+    <>
+      <Navbar bg="dark" expand="lg" fixed="top" variant="dark">
+        <Link href="/"><a className="navbar-brand">YOUR APP NAME</a></Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="mr-auto">
+            <Nav className="mr-auto">
+              <Link href="/"><a className="nav-link">Register</a></Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    </>
+  )
+}
